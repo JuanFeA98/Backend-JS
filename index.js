f0808734134735dae7507e7f554c0f61c43141f9
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-09 23:43:13 -0500

    Metodo GET con parametros tipo query

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const faker = require('faker');
 
 const app = express();
 const port = 5000;
 
+// Endpoint base GET
 app.get('/', (req, res)=>{
   res.send('Hello, express!')
 });
@@ -12,20 +14,23 @@ app.get('/nueva_ruta', (req, res)=>{
 });
 
 app.get('/productos', (req, res)=>{
-  res.json(
-    [
-      {
-        name: 'Producto 1',
-        price: 13
-      },
-      {
-        name: 'Producto 2',
-        price: 31
-      },
-    ]
-  )
+  const products = [];
+
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index=0; index<limit; index++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    })
+  }
+
+  res.json(products)
 });
 
+// Endpoint GET con un parametro
 app.get('/productos/:id', (req, res)=>{
   const { id }= req.params;
 
@@ -38,6 +43,7 @@ app.get('/productos/:id', (req, res)=>{
   )
 })
 
+// Endpoint GET con varios parametros
 app.get('/categories/:category_id/products/:product_id', (req, res)=>{
   const { category_id, product_id } = req.params;
 
@@ -48,6 +54,22 @@ app.get('/categories/:category_id/products/:product_id', (req, res)=>{
     }
   )
 })
+
+// Endpoint GET con parametros tipo query
+app.get('/users', (req, res)=>{
+  const { limit, offset } = req.query;
+
+  if (limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parametros')
+  }
+
+})
+
 
 app.listen(port, ()=>{
   console.log(`Server run in the port ${port}`)
