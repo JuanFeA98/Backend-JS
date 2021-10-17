b13ebeea579c6172dbb1f9d87a84ee6db880161f
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-17 00:41:43 -0500

    Usando middlewares de error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const routerApi = require('./routes')
+
+const { logErrors, errorHandler } = require('./middlewares/error.handler')
 
 const app = express();
 const port = 5000;
@@ -12,7 +13,10 @@ app.get('/', (req, res)=>{
   res.send('Hello, express!')
 });
 
-routerApi(app)
+routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, ()=>{
   console.log(`Server run in the port ${port}`)
--- routes/products.router.js
+++ routes/products.router.js
@@ -13,10 +13,15 @@ router.get('/', async(req, res)=>{
 
 // Endpoint GET con un parametro
 // Llamar a un producto especifico
-router.get('/:id', async(req, res)=>{
-  const { id }= req.params;
-  const product = await service.findOne(id);
-  res.json(product);
+router.get('/:id', async(req, res, next)=>{
+  try {
+    const { id }= req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+
 })
 
 // Agregar un nuevo producto
