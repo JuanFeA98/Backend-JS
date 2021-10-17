c9de8829ed5fec57304bf50994989ee5282b8c4d
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-17 13:43:46 -0500

    Middlewares

--- services/products.service.js
+++ services/products.service.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 
 class ProductsService{
 
@@ -14,7 +15,8 @@ class ProductsService{
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
-        image: faker.image.imageUrl()
+        image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean()
       });
     }
   }
@@ -33,21 +35,30 @@ class ProductsService{
     return new Promise((resolve)=>{
       setTimeout(()=>{
         resolve(this.products)
-      }, 5000);
+      }, 1000);
     })
     // return this.products;
 
   }
 
   async findOne(id){
-    return this.products.find(item=> item.id == id);
+    const product =  this.products.find(item=> item.id == id);
+
+    if (!product){
+      throw boom.notFound('Product not found');
+    }
+    if (product.isBlock){
+      throw boom.conflict('Product is blocked')
+    }
+    return product;
+
   }
 
   async update(id, changes){
     const index = this.products.findIndex(item => item.id == id);
 
     if(index === -1){
-      throw new Error('Product Not Found')
+      throw boom.notFound('Product not found');
     } else {
       const product = this.products[index];
       this.products[index] = {
@@ -63,7 +74,7 @@ class ProductsService{
     const index = this.products.findIndex(item => item.id == id);
 
     if(index === -1){
-      throw new Error('Product Not Found')
+      throw boom.notFound('Product not found');
     } else {
       this.products.splice(index, 1);
     }
