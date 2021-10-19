d122c59446adc893ecbe4f6c88ef500f12826abc
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-10-19 18:10:19 -0500

    Preparando la conexión con la BBDD

---
+++ services/users.service.js
@@ -0,0 +1,26 @@
+class UserService{
+  constructor(){}
+
+  async create(data){
+    return data
+  }
+
+  async find(){
+
+  }
+
+  async findOne(){
+
+  }
+
+  async update(){
+
+  }
+
+  async delete(){
+
+  }
+
+}
+
+module.exports = UserService;
